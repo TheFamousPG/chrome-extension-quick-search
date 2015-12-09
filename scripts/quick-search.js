4d3a49df4004d56d19bd9a6e8c8e2e92e7812fe2
mlescaudron <mathieu.lescaudron@gmail.com>
2015-12-09 18:10:04 +0100

    Improve speed faster 2X

--- scripts/quick-search.js
+++ scripts/quick-search.js
@@ -5,20 +5,18 @@ var quickSearch = {
 
     open: 0,
 
-    content: '',
-
     init: function(){
+
         $(window).keyup(function(touche){
             var key = touche.which || touche.keyCode;
             if(key == 16){// 16 = shit
                 quickSearch.open += 1;
                 if(quickSearch.open >= 2){
-                    console.log('show !');
                     quickSearch.show();
                 } else {
                     setTimeout(function(){
                         quickSearch.open = 0;
-                    },500);
+                    },250);
                 }
             }
         });
@@ -31,10 +29,9 @@ var quickSearch = {
 
     // show the popup
     show:function(){
-        quickSearch.remove();
 
+        quickSearch.remove();
         var icon = chrome.extension.getURL("images/icon128.png");
-        console.log(icon);
         $('body').prepend('<form id="chrome-extension-google-quick-search" action="https://www.google.com/search" method="GET" target="_blank"><input type="text" id="chrome-extension-google-quick-search-input" name="q" placeholder="Press Enter when finished ;)"/></form>').find('#chrome-extension-google-quick-search-input').focus().on('click',function(e){
             e.stopPropagation();
             e.preventDefault();
